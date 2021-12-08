3f2abf2d1716cca2692f94a2b54c8c7823f5d48f
Marcin Rek <mrek@squiz.pl>
2021-12-08 19:06:17 +0100

    rename jest files to *.spec.js for better visibility in project

--- jest.config.js
+++ jest.config.js
@@ -2,7 +2,7 @@ const path = require('path');
 
 module.exports = {
     testEnvironment: 'jsdom',
-    testMatch: ['**/__tests__/**/*.[jt]s?(x)', '**/__jest__/**/*.[jt]s?(x)'],
+    testMatch: ['**/__tests__/**/*.spec.[jt]s?(x)', '**/__jest__/**/*.spec.[jt]s?(x)'],
     moduleDirectories: ['node_modules', path.join(__dirname, 'src/modules'), './src', './'],
     moduleNameMapper: {
         '\\.(s?css|less)$': 'identity-obj-proxy',

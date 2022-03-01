325ff4b2b309fbe0d5100731f037999f5898feee
Marcin Rek <mrek@squiz.pl>
2022-03-01 15:36:35 +0100

    update loco-server version

--- loco_functions/scaffold.js
+++ loco_functions/scaffold.js
@@ -7,12 +7,13 @@ const description = 'Blank function scaffold.';
  * Main process function
  * @param {object} query GET request query
  * @param {object} bodyJSON POST request body
+ * @param {string} reqMethod request method
  * @param {object} loco helper functions object
  * @param {object} envVars environment variables
  * @returns {object} response object consisting of response statusCode, body and headers object
  */
 // eslint-disable-next-line no-unused-vars
-const processFunction = async (query, bodyJSON, loco, envVars) => {
+const processFunction = async (query, bodyJSON, reqMethod, loco, envVars) => {
     return {
         statusCode: 200,
         headers: loco.corsHeaders(),

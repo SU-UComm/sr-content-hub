6c7aea523022c4393623f97461badd80671587f2
Marcin Rek <mrek@squiz.pl>
2022-05-20 11:41:54 +0200

    copy inline-* svgs to build

--- webpack/webpack.common.js
+++ webpack/webpack.common.js
@@ -88,6 +88,10 @@ const copyWebPack = new CopyWebpackPlugin({
             from: path.resolve(__dirname, '../src/externals'),
             to: 'externals',
         },
+        {
+            from: 'src/modules/**/inline-*.svg',
+            to: 'svg/[name][ext]',
+        },
     ],
 });
 

22158a4f155f5f7d7f0125159e8157ac5f14b8e4
Rajoshi Chakravarty <rchakravarty@SQUIZ-2021-C02DVASXML7H.local>
2024-01-23 03:08:08 -0800

    status filter data

--- src/modules/AllContent/AllContent.jsx
+++ src/modules/AllContent/AllContent.jsx
@@ -1,33 +1,66 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
 import {PageHeading} from '../Home/Heading.jsx';
 import {StatusFilter} from '../Filters/StatusFilter.jsx';
 import {DateRangeFilter} from '../Filters/DateFilter.jsx';
 import {CPFilter} from '../Filters/CPFilter.jsx';
 
 export const AllContent = () => {
+    const [isLoading, setIsLoading] = useState(false); // Loader flag
+    const [data, setData] = useState([]); // data from endpoint
+    const [results, setResults] = useState([]); // data from endpoint
+    const [statusLabel, setStatusLabels] = useState([]); // data from endpoint
+
+    useEffect(() => {
+        setIsLoading(true);
+        const fetchData = async () => {
+            try {
+                const response = await fetch(
+                    'https://dxp-us-stage-search.funnelback.squiz.cloud/s/search.json?profile=search&collection=sug~sp-stanford-university-content-hub&num_ranks=10&start_rank=1&sort=dmetamtxCreated&&query=!nullquery',
+                );
+                if (!response.ok) {
+                    throw new Error('Network response was not ok');
+                }
+                const res = await response.json();
+                setData(res);
+                setStatusLabels(res.response.facets[1].allValues);
+                console.log('FACETS SET', statusLabel);
+                setIsLoading(false);
+
+                console.log('main data', res);
+                setResults(res.response.resultPacket.results);
+            } catch (error) {
+                console.error('Error fetching data:', error);
+            }
+        };
+        fetchData();
+        // setResults(data.response.resultPacket.results);
+    }, []);
+
     return (
-        <div className="su-col-span-full xl:su-col-start-2 xl:su-col-span-10">
-            <PageHeading headingText={window?.data?.texts?.allcontent?.headingText} subHeadingText={window?.data?.texts?.newcontent?.subHeadingText} />
-            <section>
-                <div className="su-mb-20">
-                    <div className="su-flex su-flex-col lg:su-flex-row su-gap-xs">
-                        {/* CP Filter */}
-                        <div className="su-flex-[calc(100%/3)_1_1]">
-                            <label htmlFor="cp-filter" className="su-block su-text-18 su-font-bold su-leading-[2] su-mb-10">
-                                Content partners
-                            </label>
+        !isLoading && (
+            <div className="su-col-span-full xl:su-col-start-2 xl:su-col-span-10">
+                <PageHeading headingText={window?.data?.texts?.allcontent?.headingText} subHeadingText={window?.data?.texts?.newcontent?.subHeadingText} />
+                <section>
+                    <div className="su-mb-20">
+                        <div className="su-flex su-flex-col lg:su-flex-row su-gap-xs">
+                            {/* CP Filter */}
+                            <div className="su-flex-[calc(100%/3)_1_1]">
+                                <label htmlFor="cp-filter" className="su-block su-text-18 su-font-bold su-leading-[2] su-mb-10">
+                                    Content partners
+                                </label>
 
-                            <div className="undefined">
-                                <CPFilter />
+                                <div className="undefined">
+                                    <CPFilter />
+                                </div>
                             </div>
-                        </div>
 
-                        <StatusFilter />
-                        <DateRangeFilter />
+                            <StatusFilter facets={statusLabel} />
+                            <DateRangeFilter />
+                        </div>
+                        {/* <!-- No Selected Filters --> */}
                     </div>
-                    {/* <!-- No Selected Filters --> */}
-                </div>
-            </section>
-        </div>
+                </section>
+            </div>
+        )
     );
 };
--- src/modules/Filters/StatusFilter.jsx
+++ src/modules/Filters/StatusFilter.jsx
@@ -1,17 +1,19 @@
 import React, {useState, useRef, useEffect} from 'react';
 
-export const StatusFilter = (filterType) => {
+export const StatusFilter = (facets) => {
     const [selectedStatus, setSelectedStatus] = useState('');
     const [open, setOpen] = useState(false);
     const wrapperRef = useRef(null);
+    const [statusOptions, setStatusOptions] = useState([]);
+    const [isLoading, setIsLoading] = useState(false); // Loader flag
 
-    const statusOptions = [
-        {value: '', label: 'All'},
-        {value: 'submitted', label: 'Submitted'},
-        {value: 'reviewed', label: 'Reviewed'},
-        {value: 'sent-to-sr', label: 'Publishing soon on Stanford Report'},
-        {value: 'published', label: 'Published on Stanford Report'},
-    ];
+    // let statusOptions = [
+    //     {value: '', label: 'All'},
+    //     {value: 'submitted', label: 'Submitted'},
+    //     {value: 'reviewed', label: 'Reviewed'},
+    //     {value: 'sent-to-sr', label: 'Publishing soon on Stanford Report'},
+    //     {value: 'published', label: 'Published on Stanford Report'},
+    // ];
 
     const handleStatusChange = (value) => {
         setSelectedStatus(value);
@@ -37,67 +39,130 @@ export const StatusFilter = (filterType) => {
         };
     }, []);
 
+    useEffect(() => {
+        if (facets) {
+            setStatusOptions(facets.facets);
+            setIsLoading(false);
+        } else {
+            setIsLoading(true);
+        }
+        console.log('FSCETS', facets.facets);
+    }, [facets]);
+
     return (
-        <div className="su-flex-[calc(100%/3)_1_1]">
-            <label htmlFor="status-filter" className="su-block su-text-18 su-font-bold su-leading-[2] su-mb-10">
-                Status
-            </label>
-            <div className="c-select su-min-w-full">
-                <select
-                    className="su-opacity-0 su-float-right su-border-0 su-w-0 su-h-0 su-p-0 su-m-0"
-                    name="f.hubStatus|hubStatus"
-                    id="status-filter"
-                    value={selectedStatus}
-                    onChange={(e) => handleStatusChange(e.target.value)}
-                >
-                    {statusOptions.map((option) => (
-                        <option key={option.value} value={option.value}>
-                            {option.label}
-                        </option>
-                    ))}
-                </select>
-                <div ref={wrapperRef} className={`c-wrapper su-relative su-group ${open ? 'open' : ''}`}>
-                    <button
-                        className="c-button su-transition-none group-[.open]:su-mb-1 group-[.open]:su-border-b-transparent group-[.open]:su-rounded-b-none su-leading-[3.6rem] su-pr-20 su-w-full su-text-18 su-flex su-items-center su-justify-between su-text-left su-bg-white su-font-regular hover:su-bg-white hover:su-border-gray hover:su-text-black"
-                        aria-expanded="false"
-                        onClick={handleOpen}
+        !isLoading && (
+            <div className="su-flex-[calc(100%/3)_1_1]">
+                <label htmlFor="status-filter" className="su-block su-text-18 su-font-bold su-leading-[2] su-mb-10">
+                    Status
+                </label>
+                <div className="c-select su-min-w-full">
+                    <select
+                        className="su-opacity-0 su-float-right su-border-0 su-w-0 su-h-0 su-p-0 su-m-0"
+                        name="f.hubStatus|hubStatus"
+                        id="status-filter"
+                        value={selectedStatus}
+                        onChange={(e) => handleStatusChange(e.target.value)}
                     >
-                        <span className="su-mr-10">{selectedStatus ? statusOptions.find((opt) => opt.value === selectedStatus)?.label : 'All'}</span>
-                        <svg className="su-inline group-[.open]:su-rotate-180" xmlns="http://www.w3.org/2000/svg" width="11" height="7" viewBox="0 0 12 8" fill="none">
-                            <path
-                                fillRule="evenodd"
-                                clipRule="evenodd"
-                                d="M6.41416 7.54297L11.7071 2.25007C12.0976 1.85954 12.0976 1.22638 11.7071 0.835856L11.6213 0.750069C11.2307 0.359544 10.5976 0.359545 10.207 0.75007L4.91416 6.04297L6.41416 7.54297Z"
-                                fill="#E50808"
-                            ></path>
-                            <path
-                                fillRule="evenodd"
-                                clipRule="evenodd"
-                                d="M6.41416 7.54297L7.91416 6.04297L2.62126 0.750069C2.23074 0.359544 1.59757 0.359545 1.20705 0.75007L1.12126 0.835857C0.730738 1.22638 0.730738 1.85955 1.12126 2.25007L6.41416 7.54297Z"
-                                fill="#E50808"
-                            ></path>
-                        </svg>
-                    </button>
-                    <div className="su-z-10 su-hidden group-[.open]:su-block su-overflow-y-auto su-absolute su-border-t-0 su-border su-border-gray su-w-full su-bg-white su-rounded-b su-top-full">
-                        <ul className="su-z-10 c-list su-max-h-[209px] su-overflow-y-auto su-p-0 su-list-none">
-                            {statusOptions.map((option) => (
-                                <li
-                                    key={option.value}
-                                    role="button"
-                                    data-value={option.value}
-                                    className={`su-leading-[3.6rem] su-block su-text-18 su-mb-0 su-py-8 su-pl-15 su-pr-20 hover:su-cursor-pointer hover:su-bg-gray-light ${
-                                        option.value === selectedStatus ? 'su-bg-gray-light' : ''
-                                    }`}
-                                    tabIndex="-1"
-                                    onClick={() => handleStatusChange(option.value)}
-                                >
-                                    {option.label}
-                                </li>
-                            ))}
-                        </ul>
+                        {statusOptions.map((option) => (
+                            <option key={option.value} value={option.value}>
+                                {option.label}
+                            </option>
+                        ))}
+                    </select>
+                    <div ref={wrapperRef} className={`c-wrapper su-relative su-group ${open ? 'open' : ''}`}>
+                        <button
+                            className="c-button su-transition-none group-[.open]:su-mb-1 group-[.open]:su-border-b-transparent group-[.open]:su-rounded-b-none su-leading-[3.6rem] su-pr-20 su-w-full su-text-18 su-flex su-items-center su-justify-between su-text-left su-bg-white su-font-regular hover:su-bg-white hover:su-border-gray hover:su-text-black"
+                            aria-expanded="false"
+                            onClick={handleOpen}
+                        >
+                            <span className="su-mr-10">{selectedStatus ? statusOptions.find((opt) => opt.value === selectedStatus)?.label : 'All'}</span>
+                            <svg className="su-inline group-[.open]:su-rotate-180" xmlns="http://www.w3.org/2000/svg" width="11" height="7" viewBox="0 0 12 8" fill="none">
+                                <path
+                                    fillRule="evenodd"
+                                    clipRule="evenodd"
+                                    d="M6.41416 7.54297L11.7071 2.25007C12.0976 1.85954 12.0976 1.22638 11.7071 0.835856L11.6213 0.750069C11.2307 0.359544 10.5976 0.359545 10.207 0.75007L4.91416 6.04297L6.41416 7.54297Z"
+                                    fill="#E50808"
+                                ></path>
+                                <path
+                                    fillRule="evenodd"
+                                    clipRule="evenodd"
+                                    d="M6.41416 7.54297L7.91416 6.04297L2.62126 0.750069C2.23074 0.359544 1.59757 0.359545 1.20705 0.75007L1.12126 0.835857C0.730738 1.22638 0.730738 1.85955 1.12126 2.25007L6.41416 7.54297Z"
+                                    fill="#E50808"
+                                ></path>
+                            </svg>
+                        </button>
+                        <div className="su-z-10 su-hidden group-[.open]:su-block su-overflow-y-auto su-absolute su-border-t-0 su-border su-border-gray su-w-full su-bg-white su-rounded-b su-top-full">
+                            <ul className="su-z-10 c-list su-max-h-[209px] su-overflow-y-auto su-p-0 su-list-none">
+                                {statusOptions.map((option) => (
+                                    <li
+                                        key={option.value}
+                                        role="button"
+                                        data-value={option.value}
+                                        className={`su-leading-[3.6rem] su-block su-text-18 su-mb-0 su-py-8 su-pl-15 su-pr-20 hover:su-cursor-pointer hover:su-bg-gray-light ${
+                                            option.value === selectedStatus ? 'su-bg-gray-light' : ''
+                                        }`}
+                                        tabIndex="-1"
+                                        onClick={() => handleStatusChange(option.value)}
+                                    >
+                                        {option.label}
+                                    </li>
+                                ))}
+                            </ul>
+                        </div>
                     </div>
                 </div>
             </div>
-        </div>
+        )
     );
 };
+
+// const getFormattedFacet = (facets) => {
+//     // Loop through facets :: And find CP Facet
+//     const statusFacetArr = facets.filter((el) => {
+//         const facetName = el.name;
+//         if (facetName === 'hubStatus') {
+//             return true;
+//         }
+//         return false;
+//     });
+
+//     if (statusFacetArr.length === 0) {
+//         return false;
+//     }
+//     const statusFacet = statusFacetArr[0];
+
+//     // Labels map with friendly names
+//     const labelsMap = contentHubGlobals.defaultValues.statuses;
+
+//     // Get formatted version of Content Partners Facet :: without selected values
+//     const facetsOutput = [];
+//     for (let i in statusFacet.allValues) {
+//         const thisFacet = statusFacet.allValues[i];
+
+//         // Fix Toggle URL
+//         let thisToggle = thisFacet.toggleUrl.split('&profile=')[0];
+//         thisToggle = decodeURI(thisToggle);
+//         thisToggle = thisToggle.split('f.hubStatus|hubStatus=')[1];
+//         thisToggle = 'f.hubStatus|hubStatus=' + thisToggle;
+
+//         if (thisToggle.indexOf('?') > -1) {
+//             thisToggle = '&' + thisToggle;
+//         } else {
+//             thisToggle = '?' + thisToggle;
+//         }
+
+//         // Map label name
+//         const thisLabel = thisFacet.label;
+//         const mappedLabel = labelsMap[thisLabel] || thisLabel;
+
+//         facetsOutput.push({
+//             label: mappedLabel,
+//             count: thisFacet.count,
+//             toggleUrl: thisToggle,
+//             selected: thisFacet.selected,
+//         });
+//     }
+
+//     // Return selected items
+//     return facetsOutput;
+// };

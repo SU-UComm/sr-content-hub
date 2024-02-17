d06eb720dbb173a408bb1e9f824049d282abe99d
Rajoshi Chakravarty <rchakravarty@SQUIZ-2021-C02DVASXML7H.local>
2024-02-16 16:05:16 -0800

    rebuild

--- build/js/chStory.js
+++ build/js/chStory.js
@@ -16,8 +16,6 @@ var es_regexp_exec = __webpack_require__(4916);
 var es_string_search = __webpack_require__(4765);
 // EXTERNAL MODULE: ./node_modules/core-js/modules/es.string.match.js
 var es_string_match = __webpack_require__(4723);
-// EXTERNAL MODULE: ./node_modules/core-js/modules/es.function.name.js
-var es_function_name = __webpack_require__(8309);
 // EXTERNAL MODULE: ./node_modules/core-js/modules/es.symbol.js
 var es_symbol = __webpack_require__(2526);
 // EXTERNAL MODULE: ./node_modules/core-js/modules/es.symbol.description.js
@@ -34,6 +32,8 @@ var es_string_iterator = __webpack_require__(8783);
 var web_dom_collections_iterator = __webpack_require__(3948);
 // EXTERNAL MODULE: ./node_modules/core-js/modules/es.array.slice.js
 var es_array_slice = __webpack_require__(7042);
+// EXTERNAL MODULE: ./node_modules/core-js/modules/es.function.name.js
+var es_function_name = __webpack_require__(8309);
 // EXTERNAL MODULE: ./node_modules/core-js/modules/es.array.from.js
 var es_array_from = __webpack_require__(1038);
 // EXTERNAL MODULE: ./node_modules/core-js/modules/es.promise.js
@@ -596,120 +596,7 @@ var StoryView = function StoryView() {
   }, /*#__PURE__*/react.createElement(CardButtons/* CardButtons */.G, {
     listMetadata: data.metadata,
     assetId: data.id
-  })))), /*#__PURE__*/react.createElement("section", {
-    className: "su-flex su-flex-col su-gap-[30px] su-mb-80"
-  }, /*#__PURE__*/react.createElement("div", null, /*#__PURE__*/react.createElement("p", {
-    className: "small-heading"
-  }, "Headline"), /*#__PURE__*/react.createElement("h2", {
-    className: "su-py-20 su-mb-0"
-  }, data.name)), /*#__PURE__*/react.createElement("div", null, /*#__PURE__*/react.createElement("p", {
-    className: "small-heading"
-  }, "Summary"), /*#__PURE__*/react.createElement("p", {
-    className: "su-mb-0 su-py-20 su-leading-normal"
-  }, summary)), /*#__PURE__*/react.createElement("div", null, /*#__PURE__*/react.createElement("ul", {
-    className: "su-m-0 su-p-0 su-list-none su-gap-y-[12px] sm:su-gap-y-[24px] su-gap-x-[24px] md:su-gap-x-2xl su-grid su-grid-cols-1 sm:su-grid-cols-2"
-  }, /*#__PURE__*/react.createElement("li", {
-    className: "mb-0"
-  }, /*#__PURE__*/react.createElement("p", {
-    className: "su-leading-[3.6rem] su-font-semibold su-text-16 su-mb-8"
-  }, "Main Category"), /*#__PURE__*/react.createElement("p", {
-    className: "su-leading-[3.6rem] su-mb-0"
-  })), /*#__PURE__*/react.createElement("li", {
-    className: "mb-0"
-  }, /*#__PURE__*/react.createElement("p", {
-    className: "su-leading-[3.6rem] su-font-semibold su-text-16 su-mb-8"
-  }, "Submitted by"), /*#__PURE__*/react.createElement("p", {
-    className: "su-leading-[3.6rem] su-mb-0"
-  }, data.metadata.srcContentSource)), /*#__PURE__*/react.createElement("li", {
-    className: "mb-0"
-  }, /*#__PURE__*/react.createElement("p", {
-    className: "su-leading-[3.6rem] su-font-semibold su-text-16 su-mb-8"
-  }, "Byline"), /*#__PURE__*/react.createElement("p", {
-    className: "su-leading-[3.6rem] su-mb-0"
-  }, data.metadata.bylineAuthor)), /*#__PURE__*/react.createElement("li", {
-    className: "mb-0"
-  }, /*#__PURE__*/react.createElement("p", {
-    className: "su-leading-[3.6rem] su-font-semibold su-text-16 su-mb-8"
-  }, "Submitted on"), /*#__PURE__*/react.createElement("p", {
-    className: "su-leading-[3.6rem] su-mb-0"
-  }, data.created.date)), /*#__PURE__*/react.createElement("li", {
-    className: "mb-0"
-  }, /*#__PURE__*/react.createElement("p", {
-    className: "su-leading-[3.6rem] su-font-semibold su-text-16 su-mb-8"
-  }, "Other Topics"), /*#__PURE__*/react.createElement("p", {
-    className: "su-leading-[3.6rem] su-mb-0"
-  }, data.metadata.topics.length > 0 ? data.metadata.topics : 'NA')), /*#__PURE__*/react.createElement("li", {
-    className: "mb-0"
-  }, /*#__PURE__*/react.createElement("p", {
-    className: "su-leading-[3.6rem] su-font-semibold su-text-16 su-mb-8"
-  }, "First Published"), /*#__PURE__*/react.createElement("p", {
-    className: "su-leading-[3.6rem] su-mb-0"
-  }, data.metadata.publishedDate)), /*#__PURE__*/react.createElement("li", {
-    className: "mb-0"
-  }, /*#__PURE__*/react.createElement("p", {
-    className: "su-leading-[3.6rem] su-font-semibold su-text-16 su-mb-8"
-  }, "Other keywords"), /*#__PURE__*/react.createElement("p", {
-    className: "su-leading-[3.6rem] su-mb-0"
-  }, /*#__PURE__*/react.createElement("em", null, data.metadata.srcKeywords.length > 0 ? data.metadata.srcKeywords : 'NA'))))), /*#__PURE__*/react.createElement("div", {
-    className: "su-mb-15 su-pb-45 su su-border-b su-flex su-flex-col su-gap-[10px] su-border-gray"
-  }, /*#__PURE__*/react.createElement("p", {
-    className: "small-heading su-m-0 su-p-0"
-  }, "Original URL"), /*#__PURE__*/react.createElement("p", {
-    className: "su-mb-10 su-py-20"
-  }, /*#__PURE__*/react.createElement("a", {
-    id: "story-link",
-    href: "https://engineering.stanford.edu/magazine/article/team-scientists-invent-method-modify-behavior-cells",
-    className: "su-underline hover:su-cursor-pointer su-break-words",
-    target: "_blank",
-    rel: "noreferrer"
-  }, "https://engineering.stanford.edu/magazine/article/team-scientists-invent-method-modify-behavior-cells")), /*#__PURE__*/react.createElement("div", {
-    className: "su-flex su-flex-col sm:su-flex-row su-gap-[10px]"
-  }, /*#__PURE__*/react.createElement("a", {
-    id: "story-mtx-link",
-    className: "button su-group su-flex -su-tracking-[0.176px] su-items-center su-justify-center sm:su-justify-start",
-    href: "https://sug-web.matrix.squiz.cloud/content/stories/content-partners/stanford-engineering/a-team-of-scientists-invent-a-method-to-modify-the-behavior-of-cells",
-    target: "_blank",
-    rel: "noreferrer"
-  }, "View full story", /*#__PURE__*/react.createElement("svg", {
-    className: "su-ml-5 su-transition-colors su-text-red group-hover:su-text-white",
-    width: "18",
-    height: "18",
-    fill: "none",
-    xmlns: "http://www.w3.org/2000/svg"
-  }, /*#__PURE__*/react.createElement("g", {
-    clipPath: "url(#a)"
-  }, /*#__PURE__*/react.createElement("path", {
-    fillRule: "evenodd",
-    clipRule: "evenodd",
-    d: "M4.918 4.546a.9.9 0 0 1 .9-.9h7.637a.9.9 0 0 1 .9.9v7.636a.9.9 0 0 1-1.8 0V6.718l-7.373 7.373a.9.9 0 1 1-1.273-1.272l7.373-7.373H5.818a.9.9 0 0 1-.9-.9Z",
-    fill: "currentColor"
-  })), /*#__PURE__*/react.createElement("defs", null, /*#__PURE__*/react.createElement("clipPath", {
-    id: "a"
-  }, /*#__PURE__*/react.createElement("path", {
-    fill: "#fff",
-    d: "M0 0h18v18H0z"
-  }))))), /*#__PURE__*/react.createElement("button", {
-    className: "js-copy-link su-group su-flex su-items-center -su-tracking-[0.176px] su-justify-center sm:su-justify-start"
-  }, "Copy Link", /*#__PURE__*/react.createElement("svg", {
-    className: "su-ml-5 su-transition-colors su-text-red group-hover:su-text-white",
-    xmlns: "http://www.w3.org/2000/svg",
-    width: "19",
-    height: "18",
-    viewBox: "0 0 19 18",
-    fill: "none"
-  }, /*#__PURE__*/react.createElement("path", {
-    fillRule: "evenodd",
-    clipRule: "evenodd",
-    d: "M15.6201 6.87995L14.6591 7.841C14.3036 8.19649 13.7608 8.22842 13.4448 7.91243C13.1288 7.59643 13.1608 7.05366 13.5163 6.69817L14.4394 5.77504C15.0244 5.19006 14.9875 4.20179 14.3572 3.57152C13.727 2.94125 12.7387 2.90439 12.1537 3.48938L8.48886 7.15423C7.90451 7.73857 7.94137 8.72684 8.57164 9.35711C8.88764 9.6731 8.85571 10.2159 8.50021 10.5714C8.14472 10.9269 7.60195 10.9588 7.28596 10.6428C6.02541 9.38225 6.05262 7.3048 7.34603 6.0114L11.0488 2.30862C11.0642 2.29319 11.0802 2.27833 11.0962 2.26469C12.3938 1.05272 14.4098 1.05272 15.6429 2.28583C16.876 3.51895 16.876 5.53491 15.6641 6.8326C15.6504 6.84852 15.6356 6.86452 15.6201 6.87995Z",
-    fill: "currentColor"
-  }), /*#__PURE__*/react.createElement("path", {
-    fillRule: "evenodd",
-    clipRule: "evenodd",
-    d: "M11.654 11.9889L7.9512 15.6917C7.93577 15.7071 7.91977 15.722 7.90384 15.7356C6.60616 16.9476 4.5902 16.9476 3.35708 15.7145C2.12397 14.4814 2.12397 12.4654 3.33594 11.1677C3.34958 11.1518 3.36444 11.1358 3.37987 11.1204L4.34092 10.1593C4.69641 9.80384 5.23918 9.77192 5.55518 10.0879C5.87117 10.4039 5.83924 10.9467 5.48375 11.3022L4.56063 12.2253C3.97628 12.8096 4.0125 13.7985 4.64277 14.4288C5.27304 15.0591 6.26195 15.0953 6.84629 14.511L10.5111 10.8461C11.0955 10.2618 11.0586 9.2735 10.4284 8.64323C10.1124 8.32723 10.1449 7.78382 10.4998 7.42897C10.8553 7.07347 11.3981 7.04155 11.714 7.35754C12.9746 8.61808 12.948 10.6949 11.654 11.9889Z",
-    fill: "currentColor"
-  }))))), /*#__PURE__*/react.createElement("div", {
-    className: "su-flex su-flex-col su-gap-[10px]"
-  })), /*#__PURE__*/react.createElement(FullStory, {
+  })))), /*#__PURE__*/react.createElement(FullStory, {
     data: data
   }));
 };

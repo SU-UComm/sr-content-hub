2b0dc63877ef9220c636bef5dc6a5bb11909d787
Rajoshi Chakravarty <rchakravarty@SQUIZ-2021-C02DVASXML7H.local>
2024-03-12 14:46:28 -0700

    remove home insights

--- src/modules/Home/Home.jsx
+++ src/modules/Home/Home.jsx
@@ -7,7 +7,7 @@ export const Home = () => {
     return (
         <div className="su-col-span-full xl:su-col-start-2 xl:su-col-span-10">
             <PageHeading headingText={window?.data?.texts?.home?.headingText} subHeadingText={window?.data?.texts?.home?.subHeadingText} />
-            <HomeInsights />
+            {/* <HomeInsights /> */}
             <ContentRegion />
         </div>
     );
